a28b858d839beedad44ad7f32eff5a0620189460
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-12 02:30:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,10 @@ def nyc_pigeon_organizer(data)
        # if pigeon_list[name] == nil
           pigeon_list[name] = {}
        # end
-        if pigeon_list[name][color_gender_lives] == nil
-          pigeon_list[name][color_gender_lives] = []
-        end
-        pigeon_list[name][color_gender_lives].push(stats.to_s)
+       # if pigeon_list[name][details] == nil
+          pigeon_list[name][details] = []
+      #  end
+        pigeon_list[name][details].push(stats.to_s)
       end
     end 
   end
