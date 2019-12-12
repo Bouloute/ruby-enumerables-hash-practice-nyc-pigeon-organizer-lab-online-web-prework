ccde23f64495dcfc7f7e028b78058da7da361e4c
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-12 02:35:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each{|details, value|
+    value.each{|discription, names|
+      names.each{|name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][details] == nil
+          pigeon_list[name][details] = []
+        end
+        pigeon_list[name][details].push(discription.to_s)
+      }
+    }
+  }
+  
+  pigeon_list
 end
